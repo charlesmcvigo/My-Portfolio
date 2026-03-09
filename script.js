f59797b481c632efdcec663f408f427b6590ff41
Charles Mc <charlesvigo8@gmail.com>
2026-03-10 02:31:18 +0800

    Home page done

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let title = "C.VIGO";
 let myName = "Charles Mc Vigo";
+let myIntro = "Welcome to my personal portfolio website. Here you can find information about me, my projects, and how to contact me.";
+let myViber = "+639955093733";
 
 document.getElementById("title").textContent = title;
-document.querySelector("h2").textContent = `I'm ${myName}`;
+document.getElementById("my-name").textContent = `I'm ${myName}`;
+document.getElementById("my-intro").textContent = myIntro;
+document.getElementById("viber-number").textContent = myViber;
